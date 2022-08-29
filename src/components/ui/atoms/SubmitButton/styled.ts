694d0728ee80de5fc84ef09afb59aca6f284597b
ryuna <anottrx@gmail.com>
2022-08-29 17:20:57 +0900

    ✨react-reveal 기능 추가 작업중

--- src/components/ui/atoms/SubmitButton/styled.ts
+++ src/components/ui/atoms/SubmitButton/styled.ts
@@ -9,15 +9,17 @@ interface ISubmitButton {
 
 export const SubmitBtn = styled.button<ISubmitButton>`
   width: auto;
-  height: 30px;
-  padding: 0px 10px;
+  // height: 30px;
+  padding: 8px 10px;
   font-weight: 600;
+  font-size: 1.5rem;
   color: ${({ color }) => color};
   background-color: ${({ backgroundColor }) => backgroundColor};
   cursor: pointer;
   text-align: center;
   align-items: center;
   border-radius: 20px;
+  margin-top: 8px;
   &:hover {
     color: ${({ hoverColor }) => hoverColor};
     background-color: ${({ hoverBackgroundColor }) => hoverBackgroundColor};
